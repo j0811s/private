e7248fd8b8783c813a1c261e32c14f9580e83f5b
sato <whitedog0811@hotmail.co.jp>
2022-04-12 22:38:38 +0900

    [add] entry

--- training/resources/webpack.config.js
+++ training/resources/webpack.config.js
@@ -1,18 +1,22 @@
 const webpack = require('webpack');
+const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const WebpackWatchedGlobEntries = require('webpack-watched-glob-entries-plugin');
 
 const util = require('./config');
-const entryPath = `${util.filePath.script.src}index.js`;
+const entryPath = `${util.filePath.script.src}**/*.js`;
+const ignorePath = `${util.filePath.script.src}**/_*.js`;
 const distPath = `${__dirname}/${util.filePath.script.dist}`;
+const entries = WebpackWatchedGlobEntries.getEntries([path.resolve(__dirname, entryPath)], {
+  ignore: path.resolve(__dirname, ignorePath),
+})();
 
 /**
  * 基本的な設定値
  */
 const baseOption = {
   mode: 'production',
-  entry: {
-    index: entryPath
-  },
+  entry: entries,
   optimization: {
     minimize: true,
     minimizer: [
@@ -26,7 +30,8 @@ const baseOption = {
   },
   resolve: {
     alias: {
-      npms: process.cwd() + '/node_modules'
+      npms: process.cwd() + '/node_modules',
+      module: process.cwd() + '/modules'
     }
   },
   module: {
@@ -58,7 +63,9 @@ const baseOption = {
       },
     ]
   },
-  plugins: [],
+  plugins: [
+    new WebpackWatchedGlobEntries()
+  ],
   output: {
     filename: '[name].js',
     path: distPath
