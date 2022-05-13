778a910c23708314eae045670454b798f41cbb61
sato <whitedog0811@hotmail.co.jp>
2022-05-13 20:31:46 +0900

    [delete] test-pj

--- test-pj/src/components/PublishBtn.jsx
+++
@@ -1,9 +0,0 @@
-export const PublishBtn = (props) => {
-  return (
-    <div className="btnWrap">
-      <button className="btn" type="button" onClick={() => props.onClick()}>
-        公開ボタン:{props.isPublished.toString()}
-      </button>
-    </div>
-  )
-}
--- test-pj/src/components/Title.jsx
+++
@@ -1,7 +0,0 @@
-export const Title = (props) => {
-  return (
-    <>
-      <h2>{props.title}</h2>
-    </>
-  )
-}
